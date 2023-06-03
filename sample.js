47971bbb6632c3c854b5bc08b41e320c43946cf7
unknown <kuntadashii5@gmail.com>
2023-06-03 14:56:01 +0800

    first commit

---
+++ sample.js
@@ -0,0 +1,9 @@
+//let username= window.prompt("whats yourname");
+//console.log(username);
+// let usname;
+// document.getElementById("submit").onclick=function(){
+//     usname=document.getElementById("uname").value;
+    
+// console.log(usname,typeof usname);
+
+// }
